44333268620b6497193bb2d6ce336ee19382636a
Brian Bouterse <bmbouter@gmail.com>
2010-07-21 17:08:06 -0400

    Added a view and template which will be ready to serve up the javascript

--- views.py
+++ views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def simple(request):
+    a = 0
+    return render_to_response('WebVizPy/simple.htm',
+        {'some_variable': a, },
+        context_instance=RequestContext(request))
