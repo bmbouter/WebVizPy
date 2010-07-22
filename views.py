1f7a9bd4ff1bf9e3e70782a498c33f6aca529b72
Brian Bouterse <bmbouter@gmail.com>
2010-07-22 08:57:24 -0400

    Added an additional view for the newly renamed WebVizPy/map.htm

--- views.py
+++ views.py
@@ -2,9 +2,16 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
-def simple(request):
+def timeseries(request):
     width = 800
     height = 400
     return render_to_response('WebVizPy/timeseries.htm',
+            {'width': width, 'height' : height},
+        context_instance=RequestContext(request))
+
+def map(request):
+    width = 800
+    height = 400
+    return render_to_response('WebVizPy/map.htm',
             {'width': width, 'height' : height},
         context_instance=RequestContext(request))
