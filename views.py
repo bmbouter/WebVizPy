57c0389eb064d863eefc7c8c538ef3c4deb24316
Dennis Kliban <dkliban@ncsu.edu>
2010-07-22 08:41:01 -0400

    Added template for timesieries chart

--- views.py
+++ views.py
@@ -3,7 +3,8 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 
 def simple(request):
-    a = 0
-    return render_to_response('WebVizPy/simple.htm',
-        {'some_variable': a, },
+    width = 800
+    height = 400
+    return render_to_response('WebVizPy/timeseries.htm',
+            {'width': width, 'height' : height},
         context_instance=RequestContext(request))
